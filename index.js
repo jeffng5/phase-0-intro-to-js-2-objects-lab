e6ce6d5010d7d12fee6ba754decb39bc5c679674
jeffreykng@hotmail.com <jeffreyng@Jeffreys-MBP.fios-router.home>
2023-03-18 10:01:08 -0400

    learned functions to edit obj using k:v

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee= {name : 'Jeff', streetAddress: '123 Anywhere St.'}
+
+function updateEmployeeWithKeyAndValue (x, key, value){
+    const object = {...x}
+    object[key] = value
+    return object  
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const object = {...obj}
+    delete object[key]
+    return object
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
